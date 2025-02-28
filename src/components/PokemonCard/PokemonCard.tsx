2362ade20900f0e4cce9592bd62f6aa9edd91cab
maatbm <matheusbmoraes0@gmail.com>
2025-02-28 19:56:47 -0300

    V1.0 - Declaração inicial dos arquivos

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,13 @@
+import './PokemonCardStyle';
+
+interface Props{
+    name: string;
+}
+
+export function PokemonCard({name}: Props){
+    return(
+        <div>
+            <h1>{name}</h1>
+        </div>
+    )
+}
